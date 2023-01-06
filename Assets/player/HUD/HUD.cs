5ad567709102d71111f34a473f1739de3746e4a1
hugosaintremy <61497148+hugosaintremy@users.noreply.github.com>
2023-01-06 05:37:41 +0100

    GROSSE UPDATE
    nouvelle scene + reorganisation des assets player + system player controle fini + systeme d inventaire + lampe torche + menu pause + HUD ...

---
+++ Assets/player/HUD/HUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    public GameObject flashLightON;
+    public GameObject flashLightOFF;
+    public GameObject flashLightLight;
+
+    void Start()
+    {
+        flashLightON.SetActive(false);
+        flashLightOFF.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            flashLightON.SetActive(false);
+            flashLightOFF.SetActive(true);
+        }
+        if (flashLightLight.activeInHierarchy && Input.GetButtonDown("F"))
+        {
+            flashLightON.SetActive(false);
+            flashLightOFF.SetActive(true);
+        }
+        else if (Input.GetButtonDown("F"))
+        {
+            flashLightON.SetActive(true);
+            flashLightOFF.SetActive(false);
+        }
+    }
+}
