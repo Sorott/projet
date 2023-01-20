0a2195e882fd6375274541bcb61adf6ff48016c2
hugosaintremy <61497148+hugosaintremy@users.noreply.github.com>
2023-01-20 15:59:00 +0100

    ecran mort et system de degat et de mort

--- Assets/player/playerStat.cs
+++ Assets/player/playerStat.cs
@@ -7,11 +7,42 @@ public class playerStat : MonoBehaviour
 
     [SerializeField]
     public float PlayerHP = 100f;
+    public GameObject HUD;
+    public GameObject inventaire;
+    public GameObject deathscreen;
+    public GameObject player;
+    public AudioSource SonMort;
 
+    public void Start()
+    {
+        deathscreen.SetActive(false);
+        SonMort.Stop();
+    }
 
     public void ApplyDamage(float Damage)
     {
         PlayerHP -= Damage;
+        Debug.Log("l'enemie ta fait " + Damage + " de dégats , il te reste " + PlayerHP + " de vie");
+    }
+
+    public void Update()
+    {
+        if ( PlayerHP <= 0)
+        {
+            Time.timeScale = 0;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            HUD.SetActive(false);
+            inventaire.SetActive(false);
+            deathscreen.SetActive(true);
+            SonMort.Play();
+
+        }
+
+        if (PlayerHP > 100)
+        {
+            PlayerHP = 100;
+        }
     }
 
 }
