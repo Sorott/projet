f6bea2433f1c3533276a0c2a6be58505b8bc8207
hugosaintremy <61497148+hugosaintremy@users.noreply.github.com>
2023-01-27 15:46:10 +0100

    ecran mort fix +creation scene main menu (rein dessus encore) + barre de vie (pas fini)

---
+++ Assets/player/HUD/Health/HealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private GameObject healthbar;
+    public float health;
+    private float maxhealth = 100f;
+    playerStat playerStat;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
