ddc5f53814adca96793bf931779bb817b9a3c8c8
hugosaintremy <61497148+hugosaintremy@users.noreply.github.com>
2023-02-17 15:58:06 +0100

    option menu (les menus marches pas c normal)

--- Assets/Screen/Options Menu/OptionsMenuMain.cs
+++ Assets/Screen/Options Menu/OptionsMenuMain.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class OptionsMenu : MonoBehaviour
+public class OptionsMenuMain : MonoBehaviour
 {
     public void BouttonReturnMenu()
     {
